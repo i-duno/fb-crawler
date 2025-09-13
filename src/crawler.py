e1bbbb41d7d5fd71d047b7481b247cb301637769
i-duno <allenakris0@gmail.com>
2025-09-13 12:52:22 +0800

    yes

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,4 @@
-
-#from selenium import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,12 +10,12 @@ import re
 
 import undetected_chromedriver as uc
 
-class MessengerCrawler():
+
+class MessengerCrawler:
     def __init__(self, cookies, theme):
         self.cookies = cookies
         self.browserTheme = theme
         self.driver = None
-        
 
     def inititalizeDrivers(self, flags=None):
         print("Setting up...")
@@ -31,15 +30,21 @@ class MessengerCrawler():
         wait = WebDriverWait(self.driver, 10)
         shortWait = WebDriverWait(self.driver, 3)
 
-        wait.until(EC.element_to_be_clickable((
-            By.XPATH, "//input[@name='email' and @id='email']"
-        )))
-        wait.until(EC.element_to_be_clickable((
-            By.XPATH, "//input[@name='pass' and @id='pass']"
-        )))
-        wait.until(EC.element_to_be_clickable((
-            By.XPATH, "//button[@name='login' and @id='loginbutton']"
-        )))
+        wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//input[@name='email' and @id='email']")
+            )
+        )
+        wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//input[@name='pass' and @id='pass']")
+            )
+        )
+        wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@name='login' and @id='loginbutton']")
+            )
+        )
         # Login page mustve loaded already
         print("Logging cookies...")
         for cookie in self.cookies:
@@ -51,29 +56,41 @@ class MessengerCrawler():
         self.driver.refresh()
         print("Waiting...")
         try:
-            shortWait.until(EC.element_to_be_clickable((
-                By.XPATH, "//input[@name='email' and @id='email']"
-            )))
-            shortWait.until(EC.element_to_be_clickable((
-                By.XPATH, "//input[@name='pass' and @id='pass']"
-            )))
-            shortWait.until(EC.element_to_be_clickable((
-                By.XPATH, "//button[@name='login' and @id='loginbutton']"
-            )))
+            shortWait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//input[@name='email' and @id='email']")
+                )
+            )
+            shortWait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//input[@name='pass' and @id='pass']")
+                )
+            )
+            shortWait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//button[@name='login' and @id='loginbutton']")
+                )
+            )
             print("Login page is still present. Cookies are invalid.")
             return False
         except:
             print("Passed login page.")
 
-
-        chatPinElem = wait.until(EC.element_to_be_clickable((
-            By.XPATH, "//div[@aria-label='Close' and @role='button']"
-        )))
+        chatPinElem = wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//div[@aria-label='Close' and @role='button']")
+            )
+        )
         utils.sleepRand()
         chatPinElem.click()
-        dontRestoreElem = wait.until(EC.element_to_be_clickable((
-            By.XPATH, '''//div[@aria-label="Don't restore messages" and @role='button']'''
-        )))
+        dontRestoreElem = wait.until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    """//div[@aria-label="Don't restore messages" and @role='button']""",
+                )
+            )
+        )
         utils.sleepRand()
         dontRestoreElem.click()
         utils.sleepRand(1, 1.5)
@@ -84,39 +101,36 @@ class MessengerCrawler():
 
     def interfaceWithCurrentChannel(self):
         wait = WebDriverWait(self.driver, 30)
-        self.main = wait.until(EC.element_to_be_clickable((
-            By.XPATH, "//div[@role='main']//div[@role='grid']/div/div[@role='none']"
-        )))
+        self.main = wait.until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//div[@role='main']//div[@role='grid']/div/div[@role='none']",
+                )
+            )
+        )
         self.messages = self.main.find_element(By.XPATH, "./div")
-    
+
     def listen(self):
         wait = WebDriverWait(self.driver, 9999999)
 
     def scrapeMessages(self, limit=10):
         def hasClass(elem, className):
             try:
-                return className in elem.get_attribute('class').split()
+                return className in elem.get_attribute("class").split()
             except:
                 return False
-            
 
         def findMessagesOnFrame():
             print("Waiting for page load....")
             time.sleep(10)
             print("Starting to scrape messages....")
 
-            msgs = self.messages.find_elements(
-                By.XPATH, ".//div[@role='row']"
-            )
+            msgs = self.messages.find_elements(By.XPATH, ".//div[@role='row']")
             # Drop first elem
             msgs.pop(0)
 
-            currentDt = dict(
-                month = 'NONE',
-                day = 'NONE',
-                time = 'NONE',
-                hrform = 'NONE'
-            )
+            currentDt = dict(month="NONE", day="NONE", time="NONE", hrform="NONE")
 
             rawmsg = []
             chainedmsg = []
@@ -129,37 +143,45 @@ class MessengerCrawler():
                 if not hasClass(elem, self.browserTheme):
                     # Finding text data
                     try:
-                        dateElem = elemWait.until(EC.presence_of_element_located((
-                            By.XPATH, "./div/div/div/div/h4/div[not(@aria-hidden='true')]/span"
-                        )))
+                        dateElem = elemWait.until(
+                            EC.presence_of_element_located(
+                                (
+                                    By.XPATH,
+                                    "./div/div/div/div/h4/div[not(@aria-hidden='true')]/span",
+                                )
+                            )
+                        )
                     except:
                         print("No date found.")
                         continue
-                    elemText = self.driver.execute_script("return arguments[0].innerText || window.getComputedStyle(arguments[0], '::before').getPropertyValue('content') || window.getComputedStyle(arguments[0], '::after').getPropertyValue('content');",
-                        dateElem).strip('"')
+                    elemText = self.driver.execute_script(
+                        "return arguments[0].innerText || window.getComputedStyle(arguments[0], '::before').getPropertyValue('content') || window.getComputedStyle(arguments[0], '::after').getPropertyValue('content');",
+                        dateElem,
+                    ).strip('"')
                     print("Raw text: ", elemText)
-                    date = re.sub(r'\s+', '_', elemText).split('_')
+                    date = re.sub(r"\s+", "_", elemText).split("_")
                     print(f"Processing data: ${elemText} as ${date}")
                     if len(date) == 5:
-                        currentDt['month'] = date[0]
-                        currentDt['day'] = date[1]
-                        currentDt['time'] = date[3]
-                        currentDt['hrform'] = date[4]
+                        currentDt["month"] = date[0]
+                        currentDt["day"] = date[1]
+                        currentDt["time"] = date[3]
+                        currentDt["hrform"] = date[4]
                     elif len(date) == 4:
-                        currentDt['month'] = date[0]
-                        currentDt['day'] = date[0]
-                        currentDt['time'] = date[2]
-                        currentDt['hrform'] = date[3]
+                        currentDt["month"] = date[0]
+                        currentDt["day"] = date[0]
+                        currentDt["time"] = date[2]
+                        currentDt["hrform"] = date[3]
                     else:
                         print("invalid date format.")
-                   
-                    
+
                 else:
                     # assume currentDT already exists
                     try:
-                        msgElem = elemWait.until(EC.visibility_of_element_located((
-                            By.XPATH, ".//div[@dir='auto']"
-                        )))
+                        msgElem = elemWait.until(
+                            EC.visibility_of_element_located(
+                                (By.XPATH, ".//div[@dir='auto']")
+                            )
+                        )
                     except:
                         print("Invalid message type")
                         continue
@@ -170,32 +192,41 @@ class MessengerCrawler():
                         )
                         chainedmsg.append(msgElem.text)
                         print(f"Found text {msgElem.text}")
-                        rawmsg.append({
-                            'timedata': currentDt,
-                            'message': chainedmsg,
-                            'displayname': profileElem.get_attribute('alt'),
-                            'profilesrc': profileElem.get_attribute('src')
-                        })
-                        chainedmsg = [] #Lose reference
+                        rawmsg.append(
+                            {
+                                "timedata": currentDt,
+                                "message": chainedmsg,
+                                "displayname": profileElem.get_attribute("alt"),
+                                "profilesrc": profileElem.get_attribute("src"),
+                            }
+                        )
+                        chainedmsg = []  # Lose reference
                     except:
                         chainedmsg.append(msgElem.text)
-                    
+
             return rawmsg
 
         return findMessagesOnFrame()
 
- 
-
 if __name__ == "__main__":
-    CHANNEL = 1452226939361503
+    CHANNEL = 31329598156639048
 
-    crawler = MessengerCrawler(json.load(open("cookie.js", "r")), '__fb-light-mode') # Burner account
+    crawler = MessengerCrawler(
+        json.load(open("cookie.js", "r")), "__fb-light-mode"
+    )  # Burner account
     crawler.inititalizeDrivers()
     crawler.loginMessenger()
     crawler.navigateToChannel(CHANNEL)
     crawler.interfaceWithCurrentChannel()
 
-    with open(f"./results/{CHANNEL}{time.strftime("%Y-%m-%d@%H-%M-%S",time.localtime())}", "w", encoding="utf-8") as f:
-        json.dump(crawler.scrapeMessages(), f, ensure_ascii=False, indent=2)
-
+    messages = crawler.scrapeMessages()
+    fileName = time.strftime("%Y-%m-%d@%H-%M-%S",time.localtime())
+    with open(
+        f"./results/{CHANNEL}{fileName}",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(messages, f, ensure_ascii=False, indent=2)
+    utils.dumpJsonToWebhook(CHANNEL, f"./results/{fileName}")
     input("Press Enter to end")
+
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import random
 import time
+import json
+import requests
 
 #Simulates typing by calling function while passing text given
 def simTyping(element, text):
@@ -11,3 +13,37 @@ def simTyping(element, text):
 
 def sleepRand(min=0.1, max=0.3):
     time.sleep(random.uniform(min, max))
+
+def dumpJsonToWebhook(channel, filedir):
+    with open(filedir, "r", encoding="utf-8") as f:
+        messages = json.load(f)  # list of message dicts
+
+    url = "https://discord.com/api/webhooks/1416276378139230268/rdiZL2h4NEEFby622sHj2lQGv5PHDmnH3pj2zE-dmmoxh9dynN58AeMsY0t95nJjvECW"
+
+    for v in messages:
+        print(v)
+        timedata = v["timedata"]
+        description = "\n".join(v["message"]) if isinstance(v["message"], list) else v["message"]
+
+        payload = {
+            "username": "Messenger Scraper test",
+            "avatar_url": "https://i.imgur.com/4M34hi2.png",
+            #"content": "Showing scraped messages:",
+            "embeds": [
+                {
+                    "author": {
+                        "name": v["displayname"],
+                        "icon_url": v["profilesrc"],
+                    },
+                    "title": f"{timedata['day']} at {timedata['time']} {timedata['hrform']}" if timedata["month"] == timedata["day"] else f"{timedata['month']} {timedata['day']} at {timedata['time']} {timedata['hrform']}",
+                    "description": description,
+                    "color": 28049,
+                    "footer": {
+                        "text": f"ThreadID: {channel}",
+                    },
+                }
+            ],
+        }
+
+        time.sleep(1)
+        response = requests.post(url, json=payload)
