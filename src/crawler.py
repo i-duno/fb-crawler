139a1ee1d1549327a6bbf04e0d625f11bf757ba6
i-duno <allenakris0@gmail.com>
2025-09-13 23:56:52 +0800

    improved

--- src/crawler.py
+++ src/crawler.py
@@ -1,232 +1,258 @@
-# from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
-import utils
-import json
-import time
-import re
+from selenium.common.exceptions import StaleElementReferenceException
 
 import undetected_chromedriver as uc
+import utils
 
+import json, time, hashlib
+import logging, sys, os
+import threading
+
+from typing import cast
+
+from dotenv import load_dotenv
+load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(name)s %(asctime)s [%(levelname)s]: %(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger("Crawlerv1.0")
 
 class MessengerCrawler:
-    def __init__(self, cookies, theme):
-        self.cookies = cookies
-        self.browserTheme = theme
-        self.driver = None
+    def __init__(self, flags=[]):
+        cookiePath = os.getenv("MESSENGERCOOKIE")
+        if not cookiePath:
+            logger.critical("Path to cookie invalid.")
+            sys.exit()
+        self.cookies = json.load(open(cookiePath))
 
-    def inititalizeDrivers(self, flags=None):
-        print("Setting up...")
+        self.browserTheme = None
+        self.main = None
+        self.messages = None
+        self.interfacedWithUrl = ""
+
+        self.seenMessages = []
+
+        logger.info("Starting up crawler...")
         options = uc.ChromeOptions()
-        if not flags is None:
-            options.add_argument(flags)
+        for flag in flags:
+            logger.info(f"Adding argument {flag}")
+            options.add_argument(flag)
 
         self.driver = uc.Chrome(options=options, use_subprocess=True)
+        self.loginsuccess = False
+
+    def initializeDistinctors(self, theme):
+        self.browserTheme = theme
+        logger.info("Messenger distinctors recieved")
 
     def loginMessenger(self):
+        logger.info("Entry on website")
         self.driver.get("https://www.messenger.com")
-        wait = WebDriverWait(self.driver, 10)
-        shortWait = WebDriverWait(self.driver, 3)
 
-        wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//input[@name='email' and @id='email']")
-            )
-        )
-        wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//input[@name='pass' and @id='pass']")
-            )
-        )
-        wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//button[@name='login' and @id='loginbutton']")
-            )
-        )
-        # Login page mustve loaded already
-        print("Logging cookies...")
+        # Wait for login page to load
+        utils.targetByXPATH(self.driver, "//input[@name='email' and @id='email']", 10)
+        utils.targetByXPATH(self.driver, "//input[@name='pass' and @id='pass']", 10)
+        utils.targetByXPATH(self.driver, "//button[@name='login' and @id='loginbutton']", 10)
+        logger.info("Login finished loading")
+
+        logger.info(f"Adding {len(self.cookies)} to browser.")
         for cookie in self.cookies:
             # Check for sameSite dict
             if "sameSite" in cookie:
                 cookie["sameSite"] = "None"
-            print(cookie)
             self.driver.add_cookie(cookie)
+            logger.info(f"Inserting cookie...\n{cookie}")
+        logger.info("Waiting for browser refresh...")
         self.driver.refresh()
-        print("Waiting...")
-        try:
-            shortWait.until(
-                EC.element_to_be_clickable(
-                    (By.XPATH, "//input[@name='email' and @id='email']")
-                )
-            )
-            shortWait.until(
-                EC.element_to_be_clickable(
-                    (By.XPATH, "//input[@name='pass' and @id='pass']")
-                )
-            )
-            shortWait.until(
-                EC.element_to_be_clickable(
-                    (By.XPATH, "//button[@name='login' and @id='loginbutton']")
-                )
-            )
-            print("Login page is still present. Cookies are invalid.")
-            return False
-        except:
-            print("Passed login page.")
 
-        chatPinElem = wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//div[@aria-label='Close' and @role='button']")
-            )
-        )
+        if not utils.checkIfAllFalse(
+            utils.targetByXPATH(self.driver, "//input[@name='email' and @id='email']", 3),
+            utils.targetByXPATH(self.driver, "//input[@name='pass' and @id='pass']", 3),
+        ):
+            logger.critical("Login failed, stopping program.")
+            sys.exit(1)
+        logger.info("Finalizing login...")
         utils.sleepRand()
-        chatPinElem.click()
-        dontRestoreElem = wait.until(
-            EC.element_to_be_clickable(
-                (
-                    By.XPATH,
-                    """//div[@aria-label="Don't restore messages" and @role='button']""",
-                )
-            )
-        )
+        chatelem = utils.targetByXPATH(self.driver, "//div[@aria-label='Close' and @role='button']", 10)
+        utils.executeOnTrue(chatelem, lambda: chatelem.click()) # type: ignore
         utils.sleepRand()
-        dontRestoreElem.click()
-        utils.sleepRand(1, 1.5)
+        dontrestore = utils.targetByXPATH(self.driver, """//div[@aria-label="Don't restore messages" and @role='button']""", 10)
+        utils.executeOnTrue(dontrestore, lambda: dontrestore.click()) # type: ignore
+        utils.sleepRand(3, 5)
+        logger.info("Login successful")
+
+        self.loginsuccess = True
+
         return True
 
     def navigateToChannel(self, id):
+        logger.info(f"Redirecting to https://www.messenger.com/t/{id}")
         self.driver.get(f"https://www.messenger.com/t/{id}")
 
+        ready_state = self.driver.execute_script("return document.readyState")
+        if ready_state == "complete":
+            logger.info("DOM fully loaded, waiting arbitary time for react elements to load...")
+        utils.sleepRand(3, 5)
+
     def interfaceWithCurrentChannel(self):
-        wait = WebDriverWait(self.driver, 30)
-        self.main = wait.until(
-            EC.element_to_be_clickable(
-                (
-                    By.XPATH,
-                    "//div[@role='main']//div[@role='grid']/div/div[@role='none']",
-                )
-            )
-        )
-        self.messages = self.main.find_element(By.XPATH, "./div")
+        logger.info("Interfacing with current channel...")
+        logger.info("Looking for MAIN")
+        self.main = utils.targetByXPATH(self.driver, "//div[@role='main']//div[@role='grid']/div/div[@role='none']", 15)
+        logger.info("Looking for MESSAGES")
+        self.messages = utils.targetByXPATH(self.main, "./div", 15)
+        self.interfacedWithUrl = self.driver.current_url
+        logger.info("Interface done.")
 
-    def listen(self):
-        wait = WebDriverWait(self.driver, 9999999)
+    def resetSeenMessages(self):
+        self.seenMessages = []  
 
-    def scrapeMessages(self, limit=10):
-        def hasClass(elem, className):
-            try:
-                return className in elem.get_attribute("class").split()
-            except:
-                return False
-
-        def findMessagesOnFrame():
-            print("Waiting for page load....")
-            time.sleep(10)
-            print("Starting to scrape messages....")
-
+    def scrapeMessages(self):
+        newMessages = []
+        def findNewMessagesOnFrame():
+            nonlocal newMessages
+            if not self.messages or not self.main:
+                logger.critical("Interface is empty")
+                sys.exit()
+            if self.driver.current_url != self.interfacedWithUrl:
+                logger.critical("Interface is set to another url")
+                sys.exit()
+            self.messages = cast(WebElement, self.messages)
             msgs = self.messages.find_elements(By.XPATH, ".//div[@role='row']")
-            # Drop first elem
-            msgs.pop(0)
+            
+            # Process doms
+            for elem in msgs:
+                def checkRowType():
+                    def getContent(): 
+                        if utils.hasClass(elem, self.browserTheme):
+                            #Works when it is a regular msg element
+                            msgElem = utils.targetByXPATH(elem, ".//div[@dir='auto']")
+                            if msgElem:
+                                return [msgElem.text]
 
-            currentDt = dict(month="NONE", day="NONE", time="NONE", hrform="NONE")
+                            #Check if it contains photos
+                            try:
+                                photoElems = elem.find_elements(By.XPATH, ".//img[@alt='Open photo']")
+                                if photoElems:
+                                    table = []
+                                    for individualPhotoElem in photoElems:
+                                        table.append(individualPhotoElem.get_attribute("src"))
+                                    return table
+                            finally:
+                                pass
+                        
+                            #Check if it contains gif images
+                            gifElems = utils.targetByXPATH(elem, ".//img[@alt='GIF Image']")
+                            if gifElems:
+                                return [gifElems.get_attribute("src")]
+                        
+                            #Check if it has a download link
+                            dlLink = utils.targetByXPATH(elem, ".//a[@role='link']")
+                            if dlLink:
+                                return [dlLink.get_attribute("href")]
+                            
+                            return []
+                        else: return []
 
-            rawmsg = []
-            chainedmsg = []
+                    content = getContent()
+                    
+                    profileElem = utils.targetByXPATH(elem, ".//div[@role='button']/img")
+                    profileData = {
+                        "displayName": "",
+                        "profileSrc": ""
+                    }
+                    if profileElem:
+                        profileData['displayName'] = profileElem.get_attribute("alt") or "none"
+                        profileData['profileSrc'] = profileElem.get_attribute("src") or "none"
 
-            print(f"Found ${str(len(msgs))} row elements to scrape.")
-
-            for i, elem in enumerate(msgs):
-                print(f"Processing element ${i} out of ${len(msgs)}")
-                elemWait = WebDriverWait(elem, 10)
-                if not hasClass(elem, self.browserTheme):
-                    # Finding text data
-                    try:
-                        dateElem = elemWait.until(
-                            EC.presence_of_element_located(
-                                (
-                                    By.XPATH,
-                                    "./div/div/div/div/h4/div[not(@aria-hidden='true')]/span",
-                                )
-                            )
-                        )
-                    except:
-                        print("No date found.")
+                    return (content, profileData)
+                # Check first for uuid
+                logger.debug("Processing new chat row...")
+                try:
+                    content, profileData = checkRowType()
+                    if content == []:
                         continue
-                    elemText = self.driver.execute_script(
-                        "return arguments[0].innerText || window.getComputedStyle(arguments[0], '::before').getPropertyValue('content') || window.getComputedStyle(arguments[0], '::after').getPropertyValue('content');",
-                        dateElem,
-                    ).strip('"')
-                    print("Raw text: ", elemText)
-                    date = re.sub(r"\s+", "_", elemText).split("_")
-                    print(f"Processing data: ${elemText} as ${date}")
-                    if len(date) == 5:
-                        currentDt["month"] = date[0]
-                        currentDt["day"] = date[1]
-                        currentDt["time"] = date[3]
-                        currentDt["hrform"] = date[4]
-                    elif len(date) == 4:
-                        currentDt["month"] = date[0]
-                        currentDt["day"] = date[0]
-                        currentDt["time"] = date[2]
-                        currentDt["hrform"] = date[3]
+                    #Note: content is a list and profileData is a dict
+                    msg_text = "/".join(str(x) for x in content) # type: ignore 
+                    msg_src = "/".join(str(val) for x, val in profileData.items())
+                    msg_final = "/".join([msg_text, msg_src])
+                    #msg_hash = hashlib.md5(msg_final.encode()).hexdigest()
+                    msg_hash_simple = hashlib.md5(msg_text.encode()).hexdigest()
+
+                    if msg_hash_simple in self.seenMessages:
+                        continue
                     else:
-                        print("invalid date format.")
+                        logger.info(f"Found brand new data: {content} {profileData['displayName']}")
+                        #self.seenMessages.append(msg_hash)
+                        self.seenMessages.append(msg_hash_simple) # Just store the text, since profile names may disappear
+                        newMessages.append([content, profileData])
+                    
+                except StaleElementReferenceException:
+                    logger.debug("Holding stale reference, skipping.")
+            return newMessages
 
-                else:
-                    # assume currentDT already exists
-                    try:
-                        msgElem = elemWait.until(
-                            EC.visibility_of_element_located(
-                                (By.XPATH, ".//div[@dir='auto']")
-                            )
-                        )
-                    except:
-                        print("Invalid message type")
-                        continue
+        return findNewMessagesOnFrame()
 
-                    try:
-                        profileElem = elem.find_element(
-                            By.XPATH, ".//div[@role='button']/img"
-                        )
-                        chainedmsg.append(msgElem.text)
-                        print(f"Found text {msgElem.text}")
-                        rawmsg.append(
-                            {
-                                "timedata": currentDt,
-                                "message": chainedmsg,
-                                "displayname": profileElem.get_attribute("alt"),
-                                "profilesrc": profileElem.get_attribute("src"),
-                            }
-                        )
-                        chainedmsg = []  # Lose reference
-                    except:
-                        chainedmsg.append(msgElem.text)
+from flask import Flask
+app = Flask(__name__)
 
-            return rawmsg
-
-        return findMessagesOnFrame()
+@app.route("/")
+def home():
+    return "Im alive dont kill me!"
 
 if __name__ == "__main__":
-    CHANNEL = 31329598156639048
+    app.run(host="0.0.0.0", port=10000)
 
-    crawler = MessengerCrawler(
-        json.load(open("cookie.js", "r")), "__fb-light-mode"
-    )  # Burner account
-    crawler.inititalizeDrivers("--headless")
+
+if __name__ == "__main__":
+    CHANNEL = 30974079662190426
+
+    startEmbed = utils.DiscordEmbed()
+    startEmbed.setEmbedAuthor("Web scrapper starting up.")
+    startEmbed.addEmbed(28049, f"This bot is now scrapping ID: {CHANNEL}", None, "Please stay alive, I hate making new friends. -Neon", None, None)
+    startEmbed.setFooter(f"made with hatred by fijidenzo <3")
+    logger.info(f"Sent webhook with res_code: {utils.sendDiscordWebhook(startEmbed.build(True))}")
+
+    crawler = MessengerCrawler([
+        "--headless",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--window-size=1920,1080"
+    ])
+    crawler.initializeDistinctors("__fb-light-mode")
     crawler.loginMessenger()
     crawler.navigateToChannel(CHANNEL)
     crawler.interfaceWithCurrentChannel()
+    crawler.resetSeenMessages() # remind this
 
-    messages = crawler.scrapeMessages()
-    fileName = time.strftime("%Y-%m-%d@%H-%M-%S",time.localtime())
-    with open(
-        f"./results/{CHANNEL}{fileName}",
-        "w",
-        encoding="utf-8",
-    ) as f:
-        json.dump(messages, f, ensure_ascii=False, indent=2)
-    utils.dumpJsonToWebhook(CHANNEL, f"./results/{CHANNEL}{fileName}")
-    input("Press Enter to end")
+    
 
+    def worker():
+        while True:
+            logger.info("Scrapping new wave now")
+            messages = crawler.scrapeMessages()
+
+            for msg in messages:
+                content, profileData = msg[0], msg[1]
+                embed = utils.DiscordEmbed()
+                embed.setEmbedAuthor(profileData['displayName'], crawler.driver.current_url, profileData['profileSrc'])
+                embed.setFooter(f"ID: {CHANNEL}")
+                for v in content:
+                    embed.addEmbed(28049, time.strftime("%a, %b %d %H:%M:%S"), None, v, None, None)
+
+                logger.info(f"Sent webhook with res_code: {utils.sendDiscordWebhook(embed.build())}")
+            time.sleep(10)
+    t = threading.Thread(target=worker)
+    t.daemon = True
+    t.start()
+
+    try:
+        while True:
+            time.sleep(1)  # safe; can be interrupted with Ctrl+C
+    except KeyboardInterrupt:
+        logger.info("Exiting program wows")
--- src/utils.py
+++ src/utils.py
@@ -1,49 +1,137 @@
-import random
-import time
-import json
-import requests
 
 #Simulates typing by calling function while passing text given
 def simTyping(element, text):
+    import random, time
     for char in text:
         time.sleep(random.uniform(0.1, 0.3))
         element.send_keys(char)
         if random.random() < 0.1: # more randomness
             time.sleep(random.uniform(0.3, 0.7))
 
+#Sleeps for a random time
 def sleepRand(min=0.1, max=0.3):
+    import random, time
     time.sleep(random.uniform(min, max))
 
-def dumpJsonToWebhook(channel, filedir):
-    with open(filedir, "r", encoding="utf-8") as f:
-        messages = json.load(f)  # list of message dicts
+#Target element using XPATH, returning False if not found.
+def targetByXPATH(elem, xPath, timeout=0):
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
 
-    url = "https://discord.com/api/webhooks/1416276378139230268/rdiZL2h4NEEFby622sHj2lQGv5PHDmnH3pj2zE-dmmoxh9dynN58AeMsY0t95nJjvECW"
+    time = WebDriverWait(elem, timeout)
+    findElem = None
 
-    for v in messages:
-        print(v)
-        timedata = v["timedata"]
-        description = "\n".join(v["message"]) if isinstance(v["message"], list) else v["message"]
+    try:
+        findElem = time.until(EC.element_to_be_clickable((
+            By.XPATH, xPath
+        )))
+    except:
+        findElem = False
 
-        payload = {
-            "username": "Messenger Scraper test",
-            "avatar_url": "https://i.imgur.com/4M34hi2.png",
-            #"content": "Showing scraped messages:",
-            "embeds": [
-                {
-                    "author": {
-                        "name": v["displayname"],
-                        "icon_url": v["profilesrc"],
-                    },
-                    "title": f"{timedata['day']} at {timedata['time']} {timedata['hrform']}" if timedata["month"] == timedata["day"] else f"{timedata['month']} {timedata['day']} at {timedata['time']} {timedata['hrform']}",
-                    "description": description,
-                    "color": 28049,
-                    "footer": {
-                        "text": f"ThreadID: {channel}",
-                    },
-                }
-            ],
+    return findElem
+
+#Returns true if all passed args is false
+def checkIfAllFalse(*args):
+    for value in args:
+        if value:
+            return False
+    return True
+
+#Executes function if condition is True
+def executeOnTrue(condition, func, *args, **kargs):
+    if condition:
+        func(*args, **kargs)
+
+def hasClass(elem, className):
+    try:
+        return className in elem.get_attribute("class").split()
+    except:
+        return False
+
+#Helper class for creating discord embeds
+class DiscordEmbed:
+    def __init__(self, username="neon-cat", avatar_url="https://i.imgur.com/v9BsQmo.png", content=None):
+        self.embed = {
+            "username": username,
+            "avatar_url": avatar_url,
+            "content": content,
+            "embeds": []
+        }
+        self.embedauthor = None
+        self.footer = None
+        self.fields = []
+
+    def setEmbedAuthor(self, name="author", url=None, icon_url=None):
+        self.embedauthor = {
+            "name": name,
+            "url": url,
+            "icon_url": icon_url
         }
 
-        time.sleep(1)
-        response = requests.post(url, json=payload)
+    def clearFields(self):
+        self.fields = []
+
+    def pushField(self, name="Field", value="field_value", inline=False):
+        self.fields.append({
+            "name": name,
+            "value": value,
+            "inline": inline
+        })
+
+    def setFooter(self, text="bot", icon_url=None):
+        self.footer = {
+            "text": text,
+            "icon_url": icon_url
+        }
+
+    def addEmbed(self, color=28049, title=None, url=None, description=None, thumbnail_url=None, image_url=None):
+        
+        if not thumbnail_url is None:
+            thumbnail_url = {
+                "url": thumbnail_url
+            }
+        if not image_url is None:
+            image_url = {
+                "url": image_url
+            }
+        
+        self.embed['embeds'].append({
+            "title": title,
+            "url": url,
+            "description": description,
+            "thumbnail_url": thumbnail_url,
+            "image_url": image_url,
+            "author": self.embedauthor,
+            "footer": self.footer,
+            "color": color
+        })
+
+    def build(self, printBuild=False) -> dict:
+        
+        def clean_none(d):
+            if isinstance(d, dict):
+            # Make a copy of keys to avoid modifying while iterating
+                keys_to_remove = []
+                for k, v in d.items():
+                    if v is None:
+                        keys_to_remove.append(k)
+                    else:
+                        clean_none(v)
+                for k in keys_to_remove:
+                    del d[k]
+            elif isinstance(d, list):
+                for item in d:
+                    clean_none(item)
+            return d
+        
+        clean_none(self.embed)
+        if printBuild:
+            print(self.embed)
+        return self.embed
+
+#Sends a webhook to discord with passed embed data
+def sendDiscordWebhook(embed, url="https://discord.com/api/webhooks/1416276378139230268/rdiZL2h4NEEFby622sHj2lQGv5PHDmnH3pj2zE-dmmoxh9dynN58AeMsY0t95nJjvECW"):
+    import requests
+    response = requests.post(url, json=embed)
+    return response.status_code
