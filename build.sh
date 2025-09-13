123da49db22460d1241445ff3b3b303067b2adcc
i-duno <allenakris0@gmail.com>
2025-09-14 00:08:37 +0800

    improved 2

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Chrome
-apt-get update
-apt-get install -y wget unzip xvfb libxi6 libgconf-2-4
-
-# Download Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
